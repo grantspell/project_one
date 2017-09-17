22b69d89ae9f11d6b0dfa18af821570802f3aa25
grantspell <spellgrant@gmail.com>
2017-09-17 18:05:34 -0400

    FINAL GAME LOGIC // YAS

--- main.js
+++ main.js
@@ -1,104 +1,111 @@
-// hangman
+//hangman
 $(document).ready(function () {
     console.log("ready!");
 });
-const answerWords = [
-    ["D", "O", "G"],
-    ["C", "A", "T"],
-    ["P", "U", "G"],
-    ["F", "I", "G"],
+
+//Global Variables
+let playerOutput = 0,
+    maskedWord = "",
+    matches = 0,
+    tries = 0,
+    maxTries = 6;
+uiPieces = {
+    play: $("#play"),
+    answers: $("#answers"),
+    guessForm: $("#guessForm"),
+    userGuess: $("#userGuess"),
+    guessButton: $("#guessButton"),
+    wrongLetters: $("#wrongLetters")
+},
+    playerInput = userGuess;
+
+//Secret Words
+const secretWords = [
+    'JIG',
+    'BOUNTY',
+    'WANTED',
+    'JUDGE',
+    'SCARED',
+    'RENEGADE',
+    'LAW',
+    'MAMA'
 ]
 
-var random = Math.floor((Math.random() * (answerWords.length)));
-var wordChosen = answerWords[random]; // the word to guess will be chosen from the array above
-var word = new Array(wordChosen.length);
+//Random secretWord Algorithm (global variables)
+const random = Math.floor((Math.random() * (secretWords.length)));
+let wordChosen = secretWords[random];
 
-let answerDisplay = [];
+//Button Handlers
+$(function() {;
 
-var tries = 0;
+    uiPieces.guessButton.on('click', function(e) {
+        e.preventDefault();
+        checkLetters();
+    })
 
-// every letter in the word is symbolized by an underscore in the answers div
-for (var i = 0; i < word.length; i++) {
-    word[i] = "_ ";
-}
+})
 
-// displays the secret word with underscores
-function printWord() {
-    for (var i = 0; i < word.length; i++) {
-        var answers = document.getElementById("answers");
-        var newWord = document.createTextNode(word[i]);
-        answers.appendChild(newWord);
-    }
-}
-
-let playerInput = $('#playerInput').val();
-let answers = document.getElementById("answers");
-let upperCaseLetter = playerInput.toUpperCase();
-let correct = 0;
-
-//checks if the the letter provided by the user matches one or more of the letters in the word
-var checkLetters = function (e) {
-    e.preventDefault();
-    //playerInput = e.target.value;
-    let playerInput = $('#playerInput').val();
-    //var playerInput = e.target.value; //input value
-    //var b = f.elements["userGuess"]; 
-    //var letterGiven = b.value; // the letter provided by the user
-    let upperCaseLetter = playerInput.toUpperCase();
-    for (var i = 0; i < wordChosen.length; i++) {
-        if (wordChosen[i] === upperCaseLetter) {
-            word[i] = upperCaseLetter + " ";
-            correct = true;
-            if (correct === true) {
-                let word = upperCaseLetter + " ";
-                var character = document.createTextNode(word);
-                $("#answers").append(character);
-            }
-        }
-       // upperCaseLetter = "";
-    }
-
-    //deletes the guessfield and replaces it with the new one
-    //var answers = document.getElementById("answers");
-    //answers.innerHTML = printWord();
-
-
-    //gives action for when answer is incorrect
-    if (!correct) {
-        var wrongLetters = document.getElementById("wrongLetters");
-        let playerInput = $('#playerInput').val();
-        let upperCaseLetter = playerInput.toUpperCase();
-        for (var i = 0; i < wordChosen.length; i++) {
-            if (wordChosen[i] !== upperCaseLetter) {
-            }
-        }
-        var character = document.createTextNode(" " + upperCaseLetter);
-        wrongLetters.appendChild(character);
-        tries++;
-    }
-
-    //checks if all letters have been found
-    var endOfGame = true;
-    for (var i = 0; i < word.length; i++) {
-        if (word[i] === "_ ") {
-            endOfGame = false;
-        }
-    }
-    if (endOfGame) {
-        window.alert("You win! Get out of the Gallows!");
-    }
-
-    //once you got six wrong letters, you lose
-    if (tries === 6) {
-        window.alert("You're dead, hangman.");
-    }
-}
-
+//Generate + Display Masked Word
 function init() {
-    printWord();
-}
+    for (var i = 0; i < wordChosen.length; i++) {
+        maskedWord += "_";
+    }
+    uiPieces.answers.html(maskedWord);
+    uiPieces.userGuess.focus();
+};
 
-$('#guessForm').submit(checkLetters);
+//Answer Check Function / Replace '_' With Correctly Guessed Letter
+function checkLetters () {
+    upperCaseLetter = "";
+    matches = false;
 
+    if (uiPieces.userGuess && uiPieces.userGuess.val()) {
+        upperCaseLetter = $.trim(uiPieces.userGuess.val().toUpperCase());
+    }    
 
-window.onload = init(); 
+    if (upperCaseLetter) {
+        for (let i = 0; i < maskedWord.length; ++i) {
+            if (secretWords[random].charAt(i) === upperCaseLetter) {
+                maskedWord = maskedWord.substr(0, i) + upperCaseLetter + maskedWord.substr(i + 1);
+                matches = true;
+            }
+        }
+        if (!matches) {
+            uiPieces.wrongLetters.append(upperCaseLetter + " ");
+            matches = false;
+            ++tries;
+            }
+    }
+
+        //Check Lose / Win Logic
+        if (hasUserLost()) {
+            userLost();
+        } else if (hasUserWon()) {
+            userWon();
+        } else {
+            uiPieces.answers.html(maskedWord);
+        }
+
+};
+
+    //Lose / Win Display Functions
+    function userLost() {
+        alert("You're a deadman, hangman!");
+    };
+
+    function userWon() {
+        uiPieces.answers.html(wordChosen);
+        alert("You win. Escape the gallows while you can, hangman!");
+    };
+
+    //Lose / Win Logic
+    function hasUserLost() {
+        return (tries >= maxTries);
+    };
+
+    function hasUserWon() {
+        return (maskedWord === wordChosen);
+    };
+
+    //Onload Functions
+    window.onload = init();
